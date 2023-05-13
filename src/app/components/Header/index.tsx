4a906674da005f062731cc473c602478fd9d4643
vishwa-akshat <akvs0707@gmail.com>
2023-05-13 17:30:01 +0530

    Added Header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Button from "@/app/components/Button";
+import HeaderBanner from "@/app/components/HeaderBanner";
+import BannerBlock from "@/app/components/BannerBlock";
 
 import "./style.scss";
 
@@ -12,6 +14,12 @@ export default function Header({}: Props) {
             <div className="heading-wrapper">
                 <p className="heading">WeWork + Salesforce</p>
                 <Button>Publish/Share</Button>
+            </div>
+            <div className="header-banner-wrapper">
+                <HeaderBanner />
+                <div className="banner-block-wrapper">
+                    <BannerBlock />
+                </div>
             </div>
         </section>
     );
---
+++ src/app/components/HeaderBanner/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.scss";
+
+type Props = {};
+
+export default function HeaderBanner({}: Props) {
+    return (
+        <div className="header-banner">
+            <p className="content">Workspace Proposal</p>
+        </div>
+    );
+}
