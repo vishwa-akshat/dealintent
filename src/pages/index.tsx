f86c09e921b63ab626f552ffea46397f739900f6
vishwa-akshat <akvs0707@gmail.com>
2023-05-13 12:17:49 +0530

    Added Navbar and icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
 import Head from "next/head";
+
+import Navbar from "@/app/components/Navbar";
+import Layout from "@/app/layout";
+
+import "./style.scss";
 
 export default function Home() {
     return (
-        <div>
+        <Layout>
             <Head>
                 <title>Dealintent</title>
             </Head>
-            Home
-        </div>
+            <main className="container">
+                <Navbar />
+            </main>
+        </Layout>
     );
 }
